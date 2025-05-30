6dea3cad4c5d18d6eb13eee191cab96c65fe889c
Behenck <denilsontrespa10@gmail.com>
2025-05-30 10:47:52 -0300

    adicionado flag do terserPlugin para rodar build otimizado para o plesk

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next'
+import TerserPlugin from 'terser-webpack-plugin'
 
 const nextConfig: NextConfig = {
   // output: 'export',
@@ -6,6 +7,22 @@ const nextConfig: NextConfig = {
   // trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  // desativa o minificador SWC
+  swcMinify: false,
+  webpack(config, { dev }) {
+    if (!dev) {
+      // força o Terser a rodar em um único processo
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          parallel: false,
+          terserOptions: {
+            format: { comments: false },
+          },
+        }),
+      ]
+    }
+    return config
   },
 }
 
